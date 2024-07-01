f6c6747f80331b698d026e953f96fd9a721435c6
Lollo Carpa <lorenzocarpaneto@yahoo.it>
2024-07-01 23:24:37 +0100

    Play pause and slider bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import Slider from '@mui/material/Slider';
+import { Button } from '@mui/material';
+
+
 
 function App() {
+  const [value, setValue] = React.useState<number>(0);
+  const [play, setPlay] = React.useState<boolean>(false);
+  const max = 100;
+  
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    setValue(newValue as number);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (play) setValue(value => (value + 1) % max);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [value, play]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider 
+        aria-label="Volume" 
+        value={value} 
+        onChange={handleChange}
+        max={max} />
+      <Button variant="text" onClick={() => setPlay(!play)}>{play? "Pause": "Play"}</Button>
     </div>
   );
 }
