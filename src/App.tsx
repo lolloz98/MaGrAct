978af1600bb9db9666fabb20281e6e48e634f152
Lollo Carpa <lorenzocarpaneto@yahoo.it>
2024-07-02 23:23:15 +0100

    Improved a bit UI for playing the animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,66 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import Slider from '@mui/material/Slider';
-import { Button } from '@mui/material';
+import { Box, Button, Stack } from '@mui/material';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 
 
 
 function App() {
-  const [value, setValue] = React.useState<number>(0);
+  const [tick, setTick] = React.useState<number>(0);
   const [play, setPlay] = React.useState<boolean>(false);
-  const max = 100;
+  const step = 50;
+  const maxTicks = 100;
   
   const handleChange = (event: Event, newValue: number | number[]) => {
-    setValue(newValue as number);
+    setPlay(false);
+    setTick(newValue as number);
   };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (play) setValue(value => (value + 1) % max);
-    }, 1000);
+      if (play) setTick(tick => (tick + 1) % (maxTicks + 1));
+    }, step);
     return () => clearInterval(interval);
-  }, [value, play]);
+  }, [tick, play, step]);
+
+  const getTime = (tick: number) => {
+    return step * tick / 1000;
+  }
+
+  const getFormattedTime = (tick: number) => {
+    return getTime(tick).toFixed(2);
+  }
+
+  const numberOfMarks = 10;
+  const marks = Array.from({length: numberOfMarks + 1}, (value, key) => { return {value: Math.round(maxTicks * key / numberOfMarks)}; })
 
   return (
     <div className="App">
-      <Slider 
-        aria-label="Volume" 
-        value={value} 
-        onChange={handleChange}
-        max={max} />
-      <Button variant="text" onClick={() => setPlay(!play)}>{play? "Pause": "Play"}</Button>
+      <Stack 
+        direction="row" 
+        justifyContent="space-around"
+        padding="16px"
+        alignItems="center"
+        spacing={8}
+      >
+        <Button 
+          variant="outlined" 
+          startIcon={play? <PauseCircleIcon />: <PlayCircleIcon />} 
+          onClick={() => setPlay(!play)}
+          style={{width: "150px"}}
+        >
+          {play? "Pause": "Play"}
+        </Button>
+        <Slider 
+          value={tick} 
+          onChange={handleChange}
+          marks={marks}
+          max={maxTicks} 
+          track={false}/>
+        <p style={{width: "100px"}}>{getFormattedTime(tick)}</p>
+      </Stack>
     </div>
   );
 }
