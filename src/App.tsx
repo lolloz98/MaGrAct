0facfddde738116fe5ea2b4104d50600a1e0e1c0
lorenzocarpaneto@yahoo.it <lorenzocarpaneto@yahoo.it>
2024-07-05 21:22:31 +0100

    Added canvas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Slider from '@mui/material/Slider';
 import { Button, Stack } from '@mui/material';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import { Stage } from 'react-konva';
 
 
 
@@ -12,7 +13,7 @@ function App() {
   const [play, setPlay] = React.useState<boolean>(false);
   const step = 50;
   const maxTicks = 100;
-  
+
   const handleSliderChange = (event: Event, newValue: number | number[]) => {
     setPlay(false);
     setTick(newValue as number);
@@ -34,32 +35,46 @@ function App() {
   }
 
   const numberOfMarks = 10;
-  const marks = Array.from({length: numberOfMarks + 1}, (value, key) => { return {value: Math.round(maxTicks * key / numberOfMarks)}; })
+  const marks = Array.from({ length: numberOfMarks + 1 }, (value, key) => { return { value: Math.round(maxTicks * key / numberOfMarks) }; })
 
   return (
+    // position: relative, user-select: none, width: 800px, height: 300px
     <div className="App">
-      <Stack 
-        direction="row" 
-        justifyContent="space-around"
-        padding="16px"
-        alignItems="center"
-        spacing={8}
-      >
-        <Button 
-          variant="outlined" 
-          startIcon={play? <PauseCircleIcon />: <PlayCircleIcon />} 
-          onClick={() => setPlay(!play)}
-          style={{width: "150px"}}
+      <Stack justifyContent="space-around" padding="16px">
+        <Stack alignItems="center">
+          <Stage style={{
+            userSelect: "none",
+            background: "black",
+            position: "relative",
+            width: "1066px",
+            height: "600px",
+            display: "block"
+          }} />
+        </Stack>
+        <Stack
+          direction="row"
+          justifyContent="space-around"
+          padding="16px"
+          alignItems="stretch"
+          spacing={8}
+          flexGrow={1}
         >
-          {play? "Pause": "Play"}
-        </Button>
-        <Slider 
-          value={tick} 
-          onChange={handleSliderChange}
-          marks={marks}
-          max={maxTicks} 
-          track={false}/>
-        <p style={{width: "100px"}}>{getFormattedTime(tick)}</p>
+          <Button
+            variant="outlined"
+            startIcon={play ? <PauseCircleIcon /> : <PlayCircleIcon />}
+            onClick={() => setPlay(!play)}
+            style={{ width: "150px" }}
+          >
+            {play ? "Pause" : "Play"}
+          </Button>
+          <Slider
+            value={tick}
+            onChange={handleSliderChange}
+            marks={marks}
+            max={maxTicks}
+            track={false} />
+          <p style={{ width: "100px" }}>{getFormattedTime(tick)}</p>
+        </Stack>
       </Stack>
     </div>
   );
