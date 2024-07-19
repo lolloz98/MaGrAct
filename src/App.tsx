ec7dafd7fad278404f7ee0e974ff4ad4999bbaf0
lorenzocarpaneto@yahoo.it <lorenzocarpaneto@yahoo.it>
2024-07-19 22:34:54 +0100

    Refactored to put timeline in separate component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React, { useEffect, ReactElement } from 'react';
 import './App.css';
 import Slider from '@mui/material/Slider';
 import { Box, Button, createTheme, CssBaseline, PaletteMode, Stack, ThemeProvider } from '@mui/material';
-import PlayCircleIcon from '@mui/icons-material/PlayCircle';
-import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 import { Layer } from 'react-konva';
 import { StageWithReactiveDimen } from './components/graphic/StageWithReactiveDimen';
 import { TimeContext } from './components/TimeContext';
@@ -16,6 +14,7 @@ import MyKatex from './components/graphic/MyKatex';
 import ListOfControls from './components/controls/ListOfControls';
 import BaseState from './components/states/BaseState';
 import TitleList from './components/titles/TitleList';
+import MyTimeline from './components/Timeline';
 
 const getDesignTokens = (mode: PaletteMode) => ({
   palette: {
@@ -60,7 +59,7 @@ function reducer(
   draft: Draft<MyStore>,
   action: StoreAction
 ) {
-  console.trace(action);
+  console.debug(action);
   switch (action.type) {
     case 'reset':
       return rawReturn(initState);
@@ -111,34 +110,12 @@ function App() {
   const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
   const [tick, setTick] = React.useState<number>(0);
-  const [play, setPlay] = React.useState<boolean>(false);
   const step = 50;
-  const maxTicks = 100;
+
   const { state, dispacth } = useMyMutative();
-  console.log(JSON.stringify(state));
-
-  const handleSliderChange = (event: Event, newValue: number | number[]) => {
-    setPlay(false);
-    setTick(newValue as number);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (play) setTick(tick => (tick + 1) % (maxTicks + 1));
-    }, step);
-    return () => clearInterval(interval);
-  }, [tick, play, step]);
-
   const getTime = (tick: number) => {
     return step * tick / 1000;
   }
-
-  const getFormattedTime = (tick: number) => {
-    return getTime(tick).toFixed(2);
-  }
-
-  const numberOfMarks = 10;
-  const marks = Array.from({ length: numberOfMarks + 1 }, (value, key) => { return { value: Math.round(maxTicks * key / numberOfMarks) }; })
 
   const children = [];
   const modifiers: ReactElement[] = [];
@@ -162,18 +139,17 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <TimeContext.Provider value={getTime(tick)}>
       <CssBaseline />
       <main className="App">
         <Stack justifyContent="space-around" padding="16px">
           <Stack alignItems="center" justifyContent="space-around" direction="row">
-            <TimeContext.Provider value={getTime(tick)}>
               <StageWithReactiveDimen dispatch={dispacth}>
                 <Layer>
                   <MyKatex></MyKatex>
                   {children}
                 </Layer>
               </StageWithReactiveDimen>
-            </TimeContext.Provider>
             <Stack direction={"column"}>
               <ListOfControls>
                 {modifiers}
@@ -181,30 +157,7 @@ function App() {
               <TitleList tree={tree} dispatch={dispacth}/>
             </Stack>
           </Stack>
-          <Stack
-            direction="row"
-            justifyContent="space-around"
-            padding="16px"
-            alignItems="stretch"
-            spacing={8}
-            flexGrow={1}
-          >
-            <Button
-              variant="outlined"
-              startIcon={play ? <PauseCircleIcon /> : <PlayCircleIcon />}
-              onClick={() => setPlay(!play)}
-              style={{ width: "150px" }}
-            >
-              {play ? "Pause" : "Play"}
-            </Button>
-            <Slider
-              value={tick}
-              onChange={handleSliderChange}
-              marks={marks}
-              max={maxTicks}
-              track={false} />
-            <p style={{ width: "100px" }}>{getFormattedTime(tick)}</p>
-          </Stack>
+          <MyTimeline tick={tick} setTick={setTick} step={step} />
           <Button
             variant="outlined"
             onClick={() => dispacth({
@@ -217,6 +170,7 @@ function App() {
           </Button>
         </Stack>
       </main>
+      </TimeContext.Provider>
     </ThemeProvider>
   );
 }
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,60 @@
+import { Button, Slider, Stack } from "@mui/material";
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
+import React, { useContext, useEffect } from "react";
+import { TimeContext } from "./TimeContext";
+
+export default function MyTimeline({ tick, setTick, step }: {
+    tick: number,
+    setTick: React.Dispatch<React.SetStateAction<number>>,
+    step: number
+}) {
+    const [play, setPlay] = React.useState<boolean>(false);
+    const time = useContext(TimeContext);
+    const maxTicks = 100;
+    const numberOfMarks = 10;
+    const marks = Array.from({ length: numberOfMarks + 1 }, (value, key) => { return { value: Math.round(maxTicks * key / numberOfMarks) }; })
+
+    const getFormattedTime = () => {
+        return time.toFixed(2);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (play) setTick(tick => (tick + 1) % (maxTicks + 1));
+        }, step);
+        return () => clearInterval(interval);
+    }, [tick, play, step]);
+
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+        setPlay(false);
+        setTick(newValue as number);
+    };
+
+    return (
+        <Stack
+            direction="row"
+            justifyContent="space-around"
+            padding="16px"
+            alignItems="stretch"
+            spacing={8}
+            flexGrow={1}
+        >
+            <Button
+                variant="outlined"
+                startIcon={play ? <PauseCircleIcon /> : <PlayCircleIcon />}
+                onClick={() => setPlay(!play)}
+                style={{ width: "150px" }}
+            >
+                {play ? "Pause" : "Play"}
+            </Button>
+            <Slider
+                value={tick}
+                onChange={handleSliderChange}
+                marks={marks}
+                max={maxTicks}
+                track={false} />
+            <p style={{ width: "100px" }}>{getFormattedTime()}</p>
+        </Stack>
+    );
+}
