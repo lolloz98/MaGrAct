ba3cba7bcaf6b14e750854bbbe4a5b8bf7543e35
lorenzocarpaneto@yahoo.it <lorenzocarpaneto@yahoo.it>
2024-07-21 02:26:07 +0100

    Sharing logic for controls

--- src/components/controls/FunctionControl.tsx
+++ src/components/controls/FunctionControl.tsx
@@ -1,45 +1,13 @@
-import { Button, IconButton, Input, Stack, TextField } from "@mui/material";
-import DeleteIcon from '@mui/icons-material/Delete';
+import { Stack } from "@mui/material";
 import { DispactherAction, MyStore } from "../StoreContext";
-import { useEffect, useState } from "react";
-import { isNumeric } from "../Utils";
 import FunctionState from "../states/FunctionState";
-import MyCustomInput from "./MyCustomInput";
+import { SharedBaseControl, SharedTitleControl } from "./SharedControl";
 
 export default function FunctionControl({ state, dispatch }: { state: FunctionState, dispatch: DispactherAction }) {
     return (
         <Stack border={1} borderRadius={2}>
-            <Button startIcon={<DeleteIcon />} onClick={() => dispatch({ type: "delete", id: state.id })}/>
-            <MyCustomInput label="Title" variant="outlined" onMyChange={
-                (e) => {
-                    const val = (e.target as HTMLInputElement).value;
-                    const newState = { ...state };
-                    newState.title = val;
-                    dispatch({ type: 'modify', state: newState });
-                }
-            } state={`${state.title}`} />
-            <Stack direction={"row"} spacing={1}>
-                <MyCustomInput label="Position_X" variant="outlined" onMyChange={
-                    (e) => {
-                        const val = (e.target as HTMLInputElement).value;
-                        if (isNumeric(val)) {
-                            const newState = { ...state };
-                            newState.position.x = +val;
-                            dispatch({ type: 'modify', state: newState });
-                        }
-                    }
-                } state={`${state.position.x}`} />
-                <MyCustomInput label="Position_Y" variant="outlined" onMyChange={
-                    (e) => {
-                        const val = (e.target as HTMLInputElement).value;
-                        if (isNumeric(val)) {
-                            const newState = { ...state };
-                            newState.position.y = +val;
-                            dispatch({ type: 'modify', state: newState });
-                        }
-                    }
-                } state={`${state.position.y}`} />
-            </Stack>
+            <SharedTitleControl state={state} dispatch={dispatch}/>
+            <SharedBaseControl state={state} dispatch={dispatch}/>
         </Stack>
     )
 }
---
+++ src/components/controls/SharedControl.tsx
@@ -0,0 +1,59 @@
+import { Button, Collapse, Stack } from "@mui/material";
+import MyCustomInput from "./MyCustomInput";
+import BaseState from "../states/BaseState";
+import { DispactherAction } from "../StoreContext";
+import { useState } from "react";
+import { isNumeric } from "../Utils";
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+export function SharedBaseControl({ state, dispatch }: { state: BaseState, dispatch: DispactherAction }) {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Stack>
+            <Button size="small" startIcon={open ? <ExpandLess /> : <ExpandMore />} onClick={() => setOpen(!open)}>Base Controls</Button>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+                <Stack direction={"row"} spacing={1}>
+                    <MyCustomInput label="Position_X" variant="outlined" onMyChange={
+                        (e) => {
+                            const val = (e.target as HTMLInputElement).value;
+                            if (isNumeric(val)) {
+                                const newState = { ...state };
+                                newState.position.x = +val;
+                                dispatch({ type: 'modify', state: newState });
+                            }
+                        }
+                    } state={`${state.position.x}`} />
+                    <MyCustomInput label="Position_Y" variant="outlined" onMyChange={
+                        (e) => {
+                            const val = (e.target as HTMLInputElement).value;
+                            if (isNumeric(val)) {
+                                const newState = { ...state };
+                                newState.position.y = +val;
+                                dispatch({ type: 'modify', state: newState });
+                            }
+                        }
+                    } state={`${state.position.y}`} />
+                </Stack>
+            </Collapse>
+        </Stack>
+    )
+}
+
+export function SharedTitleControl({ state, dispatch }: { state: BaseState, dispatch: DispactherAction }) {
+    return (
+        <Stack direction={"column"}>
+            <Button startIcon={<DeleteIcon />} onClick={() => dispatch({ type: "delete", id: state.id })} />
+            <MyCustomInput label="Title" variant="outlined" onMyChange={
+                (e) => {
+                    const val = (e.target as HTMLInputElement).value;
+                    const newState = { ...state };
+                    newState.title = val;
+                    dispatch({ type: 'modify', state: newState });
+                }
+            } state={`${state.title}`} />
+        </Stack>
+    )
+}
