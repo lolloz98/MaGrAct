55f8656adcaa786d13b57d2362e80361c21584eb
lorenzocarpaneto@yahoo.it <lorenzocarpaneto@yahoo.it>
2024-07-21 13:52:48 +0100

    Adding Numeric input

--- src/components/controls/SharedControl.tsx
+++ src/components/controls/SharedControl.tsx
@@ -7,6 +7,7 @@ import { isNumeric } from "../Utils";
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import DeleteIcon from '@mui/icons-material/Delete';
+import MyNumbericInput from "../inputs/MyNumericInput";
 
 export function SharedBaseControl({ state, dispatch }: { state: BaseState, dispatch: DispactherAction }) {
     const [open, setOpen] = useState(false);
@@ -17,70 +18,34 @@ export function SharedBaseControl({ state, dispatch }: { state: BaseState, dispa
             <Collapse in={open} timeout="auto" unmountOnExit>
                 <Stack direction={"column"} spacing={1}>
                     <Stack direction={"row"} spacing={1}>
-                        <MyCustomInput label="Position_X" variant="outlined" onMyChange={
-                            (e) => {
-                                const val = (e.target as HTMLInputElement).value;
-                                if (isNumeric(val)) {
-                                    const newState = { ...state };
-                                    newState.position.x = +val;
-                                    dispatch({ type: 'modify', state: newState });
-                                }
-                            }
-                        } state={`${state.position.x}`} />
-                        <MyCustomInput label="Position_Y" variant="outlined" onMyChange={
-                            (e) => {
-                                const val = (e.target as HTMLInputElement).value;
-                                if (isNumeric(val)) {
-                                    const newState = { ...state };
-                                    newState.position.y = +val;
-                                    dispatch({ type: 'modify', state: newState });
-                                }
-                            }
-                        } state={`${state.position.y}`} />
+                        <MyNumbericInput label="Position_X" variant="outlined" 
+                         dispatch={dispatch}
+                         state={state} get={() => state.position.x} 
+                         set={(a: BaseState, n: number) => a.position.x = n} />
+                        <MyNumbericInput label="Position_Y" variant="outlined" 
+                         dispatch={dispatch}
+                         state={state} get={() => state.position.y} 
+                         set={(a: BaseState, n: number) => a.position.y = n} />
                     </Stack>
                     <Stack direction={"row"} spacing={1}>
-                        <MyCustomInput label="Scale_X" variant="outlined" onMyChange={
-                            (e) => {
-                                const val = (e.target as HTMLInputElement).value;
-                                if (isNumeric(val)) {
-                                    const newState = { ...state };
-                                    newState.scale.x = +val;
-                                    dispatch({ type: 'modify', state: newState });
-                                }
-                            }
-                        } state={`${state.scale.x}`} />
-                        <MyCustomInput label="Scale_Y" variant="outlined" onMyChange={
-                            (e) => {
-                                const val = (e.target as HTMLInputElement).value;
-                                if (isNumeric(val)) {
-                                    const newState = { ...state };
-                                    newState.scale.y = +val;
-                                    dispatch({ type: 'modify', state: newState });
-                                }
-                            }
-                        } state={`${state.scale.y}`} />
+                        <MyNumbericInput label="Scale_X" variant="outlined" 
+                         dispatch={dispatch}
+                         state={state} get={() => state.scale.x} 
+                         set={(a: BaseState, n: number) => a.scale.x = n} />
+                        <MyNumbericInput label="Scale_Y" variant="outlined" 
+                         dispatch={dispatch}
+                         state={state} get={() => state.scale.y} 
+                         set={(a: BaseState, n: number) => a.scale.y = n} />
                     </Stack>
                     <Stack direction={"row"} spacing={1}>
-                        <MyCustomInput label="Time_Start" variant="outlined" onMyChange={
-                            (e) => {
-                                const val = (e.target as HTMLInputElement).value;
-                                if (isNumeric(val)) {
-                                    const newState = { ...state };
-                                    newState.time_constraint.start = +val;
-                                    dispatch({ type: 'modify', state: newState });
-                                }
-                            }
-                        } state={`${state.time_constraint.start}`} />
-                        <MyCustomInput label="Time_End" variant="outlined" onMyChange={
-                            (e) => {
-                                const val = (e.target as HTMLInputElement).value;
-                                if (isNumeric(val)) {
-                                    const newState = { ...state };
-                                    newState.time_constraint.end = +val;
-                                    dispatch({ type: 'modify', state: newState });
-                                }
-                            }
-                        } state={`${state.time_constraint.end}`} />
+                        <MyNumbericInput label="Time_Start" variant="outlined" 
+                         dispatch={dispatch}
+                         state={state} get={() => state.time_constraint.start} 
+                         set={(a: BaseState, n: number) => a.time_constraint.start = n} />
+                        <MyNumbericInput label="Time_End" variant="outlined" 
+                         dispatch={dispatch}
+                         state={state} get={() => state.time_constraint.end} 
+                         set={(a: BaseState, n: number) => a.time_constraint.end = n} />
                     </Stack>
                 </Stack>
             </Collapse>
---
+++ src/components/inputs/MyNumericInput.tsx
@@ -0,0 +1,32 @@
+import { TextFieldProps, TextFieldVariants } from "@mui/material";
+import MyCustomInput from "./MyCustomInput";
+import BaseState from "../states/BaseState";
+import { isNumeric } from "../Utils";
+import { DispactherAction } from "../StoreContext";
+
+export default function MyNumbericInput<Variant extends TextFieldVariants>(
+    props: {
+        /**
+         * The variant to use.
+         * @default 'outlined'
+         */
+        variant?: Variant;
+    } & Omit<TextFieldProps, 'variant'> & 
+    { state: BaseState, get: (state: BaseState) => number, 
+        set: (state: BaseState, a: number) => void, dispatch: DispactherAction }
+): JSX.Element {
+    const { get, set, dispatch, state, ...rest } = props;
+
+    const onMyChange = (e: Event) => {
+        const val = (e.target as HTMLInputElement).value;
+        if (isNumeric(val)) {
+            const newState = { ...state };
+            set(newState, +val);
+            dispatch({ type: 'modify', state: newState });
+        }
+    }
+
+    return (
+        <MyCustomInput {...rest} variant={props.variant} state={`${get(props.state)}`} onMyChange={onMyChange} />
+    );
+}
