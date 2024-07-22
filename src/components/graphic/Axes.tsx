51ab74ac401a7299e6f48c837d9f2c2f112e0cf1
lorenzocarpaneto@yahoo.it <lorenzocarpaneto@yahoo.it>
2024-07-22 22:00:05 +0100

    Added marks into function

--- src/components/graphic/Axes.tsx
+++ src/components/graphic/Axes.tsx
@@ -1,12 +1,24 @@
-import { Circle, Group, Line, Text } from "react-konva";
+import { Group, Line, Rect } from "react-konva";
 import { useContext } from 'react';
 import { TimeContext } from '../TimeContext';
 import { DispactherAction } from "../StoreContext";
-import BaseState from "../states/BaseState";
 import { computeColorDissolvenceAnimation, getCommonProps, getDraggableProps, getLineColorProps, getPositionAndScaleProps, getPositionProps, getScaleProps } from "../Utils";
 import FunctionState from "../states/FunctionState";
-import { abs, all, create, evaluate, isNaN, Matrix, number } from "mathjs";
+import { evaluate, isNaN } from "mathjs";
+import { visibility } from "html2canvas/dist/types/css/property-descriptors/visibility";
 
+
+function computeMarksPos(min: number, max: number, each: number, visible: boolean) {
+    if (!visible) return [];
+    const marks = [];
+    for (let i = -each; i >= min; i -= each) {
+        marks.push(i);
+    }
+    for (let i = each; i <= max; i += each) {
+        marks.push(i);
+    }
+    return marks
+}
 
 export default function Axes({ state, dispatch }: { state: FunctionState, dispatch: DispactherAction }) {
     const x_bounds = state.x_axis.bounds;
@@ -35,20 +47,21 @@ export default function Axes({ state, dispatch }: { state: FunctionState, dispat
             break;
         }
     }
-    console.log(points_of_points);
+
+    const commonProps = getCommonProps(state, t);
 
     const groupProps = {
         ...getDraggableProps(state, dispatch),
-        ...getCommonProps(state, t),
+        ...commonProps,
         ...getPositionProps(state)
     };
 
     const axisCommonProps = {
-        ...getCommonProps(state, t)
+        ...commonProps
     }
 
     const fnProps = {
-        ...getCommonProps(state, t),
+        ...commonProps,
         ...getLineColorProps(state, t),
         ...getScaleProps(state)
     }
@@ -63,32 +76,72 @@ export default function Axes({ state, dispatch }: { state: FunctionState, dispat
             key={i}
         />))
     }
-    console.log(fns);
+
+    const x_col = computeColorDissolvenceAnimation(state, t, (state) => (state as FunctionState).x_axis.color);
+    const y_col = computeColorDissolvenceAnimation(state, t, (state) => (state as FunctionState).y_axis.color);
+    const x_marks_pos = computeMarksPos(state.x_bounds.min, state.x_bounds.max, state.x_axis.marks.each, state.x_axis.marks.visible)
+    const y_marks_pos = computeMarksPos(state.y_bounds.min, state.y_bounds.max, state.y_axis.marks.each, state.y_axis.marks.visible)
+
+    const x_marks = [];
+    const y_marks = [];
+    const mark_l = 40;
+
+    for (let i = 0; i < x_marks_pos.length; i++) {
+        x_marks.push((<Rect 
+            {...commonProps}
+            stroke={x_col}
+            y={- mark_l / 2}
+            x={x_marks_pos[i] * state.scale.x}
+            strokeWidth={4 / state.scale.x}
+            height={mark_l} 
+            visible={state.x_axis.marks.visible}
+            scaleX={state.scale.x}
+            key={i} />))
+    }
+
+    for (let i = 0; i < y_marks_pos.length; i++) {
+        y_marks.push((<Rect 
+            {...commonProps}
+            stroke={y_col}
+            x={0}
+            y={y_marks_pos[i] * state.scale.y}
+            strokeWidth={mark_l}
+            height={4 / state.scale.y} 
+            visible={state.y_axis.marks.visible}
+            scaleY={state.scale.y}
+            key={i} />))
+    }
 
     // todo remove nest
     return (<Group
         {...groupProps}
     >   
-        <Line
-            {...axisCommonProps}
-            visible={state.x_axis.visible}
-            points={x_axis}
-            stroke={computeColorDissolvenceAnimation(state, t, (state) => (state as FunctionState).x_axis.color)}
-            scaleX={state.scale.x}
-            scaleY={state.x_axis.thickness}
-            x={0}
-            y={0}
-        />
-        <Line
-            {...axisCommonProps}
-            visible={state.y_axis.visible}
-            points={y_axis}
-            stroke={computeColorDissolvenceAnimation(state, t, (state) => (state as FunctionState).y_axis.color)}
-            scaleX={state.y_axis.thickness}
-            scaleY={state.scale.y}
-            x={0}
-            y={0}
-        />
-        {fns}
+        <Group {...commonProps} visible={state.x_axis.visible}>
+            <Line
+                {...axisCommonProps}
+                points={x_axis}
+                stroke={x_col}
+                scaleX={state.scale.x}
+                scaleY={state.x_axis.thickness}
+                x={0}
+                y={0}
+            />
+            {x_marks}
+        </Group>
+        <Group {...commonProps} visible={state.y_axis.visible}>
+            <Line
+                {...axisCommonProps}
+                points={y_axis}
+                stroke={y_col}
+                scaleX={state.y_axis.thickness}
+                scaleY={state.scale.y}
+                x={0}
+                y={0}
+            />
+            {y_marks}
+        </Group>
+        <Group>
+            {fns}
+        </Group>
     </Group>);
 }
