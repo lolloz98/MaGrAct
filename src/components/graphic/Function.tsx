695fe5bb5ea4cc4ec668aeb0b9df0606f2520fef
lorenzocarpaneto@yahoo.it <lorenzocarpaneto@yahoo.it>
2024-07-25 01:16:18 +0100

    Made code for marks better

--- src/components/graphic/Function.tsx
+++ src/components/graphic/Function.tsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { TimeContext } from '../TimeContext';
 import { DispactherAction } from "../StoreContext";
 import { computeColorDissolvenceAnimation, getCommonProps, getDraggableProps, getLineColorProps, getPositionAndScaleProps, getPositionProps, getScaleProps } from "../Utils";
-import FunctionState from "../states/FunctionState";
+import FunctionState, { Axis } from "../states/FunctionState";
 import { compile, evaluate, isNaN } from "mathjs";
 
 
@@ -18,6 +18,8 @@ function computeMarksPos(min: number, max: number, each: number, visible: boolea
     }
     return marks
 }
+
+
 
 export default function Function({ state, dispatch }: { state: FunctionState, dispatch: DispactherAction }) {
     const x_bounds = state.x_axis.bounds;
@@ -81,39 +83,29 @@ export default function Function({ state, dispatch }: { state: FunctionState, di
 
     const x_col = computeColorDissolvenceAnimation(state, t, (state) => (state as FunctionState).x_axis.color);
     const y_col = computeColorDissolvenceAnimation(state, t, (state) => (state as FunctionState).y_axis.color);
-    const x_marks_pos = computeMarksPos(state.x_axis.bounds.min, state.x_axis.bounds.max, state.x_axis.marks.each, state.x_axis.marks.visible)
-    const y_marks_pos = computeMarksPos(state.y_axis.bounds.min, state.y_axis.bounds.max, state.y_axis.marks.each, state.y_axis.marks.visible)
 
-    const x_marks = [];
-    const y_marks = [];
-    const mark_width = 40;
-    const mark_thick = 4;
-
-    for (let i = 0; i < x_marks_pos.length; i++) {
-        x_marks.push((<Rect 
-            {...commonProps}
-            stroke={x_col}
-            y={- mark_width / 2}
-            x={(x_marks_pos[i]) * state.x_axis.unit_scale}
-            strokeWidth={mark_thick / state.x_axis.unit_scale}
-            height={mark_width} 
-            visible={state.x_axis.marks.visible}
-            scaleX={state.x_axis.unit_scale}
-            key={i} />))
+    function computeMarks(axis: Axis) {
+        const mark_pos = computeMarksPos(axis.bounds.min, axis.bounds.max, axis.marks.each, axis.marks.visible);
+        const thick = axis.thickness;
+        const mark_width = axis.thickness * 6;
+        const marks = [];
+        for (let i = 0; i < mark_pos.length; i++) {
+            marks.push((<Rect 
+                {...commonProps}
+                stroke={x_col}
+                y={- mark_width / 2}
+                x={(mark_pos[i]) * axis.unit_scale}
+                strokeWidth={thick / axis.unit_scale}
+                height={mark_width} 
+                visible={axis.marks.visible}
+                scaleX={axis.unit_scale}
+                key={i} />))
+        }
+        return marks;
     }
 
-    for (let i = 0; i < y_marks_pos.length; i++) {
-        y_marks.push((<Rect 
-            {...commonProps}
-            stroke={y_col}
-            y={- mark_width / 2}
-            x={(y_marks_pos[i]) * state.y_axis.unit_scale}
-            strokeWidth={mark_thick / state.y_axis.unit_scale}
-            height={mark_width} 
-            visible={state.y_axis.marks.visible}
-            scaleX={state.y_axis.unit_scale}
-            key={i} />))
-    }
+    const x_marks = computeMarks(state.x_axis);
+    const y_marks = computeMarks(state.y_axis);
 
     // todo remove nest
     return (<Group
